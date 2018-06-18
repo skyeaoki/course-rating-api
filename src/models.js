0d699835a4e1317f4279bd56a6055171afbd9793
Skye <skyeaoki@live.com>
2018-06-18 10:27:43 -0700

    created user schema and model

---
+++ src/models.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  _id: {
+    type: Schema.ObjectId,
+    auto: true,
+    required: true
+  },
+  fullName: {
+    type: String,
+    required: true
+  },
+  emailAddress: {
+    type: String,
+    required: true,
+    unique: true,
+    // must be in valid email format
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+// Create a pre save hook on the user schema that uses the bcrypt npm package to hash the user's password.
+
+module.exports.User = User;
