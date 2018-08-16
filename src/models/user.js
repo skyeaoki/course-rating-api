1e01ed2164f64ff1ccffdcb7610152cdccb119f1
Skye <skyeaoki@live.com>
2018-08-16 13:19:06 -0700

    fixed syntax errors

--- src/models/user.js
+++ src/models/user.js
@@ -62,7 +62,7 @@ UserSchema.statics.authenticate = (email, password, callback) => {
 };
 
 // Hash password before saving to database
-UserSchema.pre('save', next => {
+UserSchema.pre('save', function(next) {
   let user = this;
   bcrypt.hash(user.password, 10, (err, hash) => {
     if(err) return next(err);
